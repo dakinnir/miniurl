9a62d6654c8035285d74be19cfd284df087eced3
Daniel Akinniranye <dakinnir@gmail.com>
2024-07-31 14:19:20 -0400

    new project setup

---
+++ src/test/java/com/repo/MiniurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
